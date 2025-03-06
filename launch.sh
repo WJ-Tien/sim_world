e5f14b637634eb072199fc580a10f06e6ac3df8e
WJ-Tien <chc61394@gmail.com>
2025-03-06 19:24:18 +0800

    update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
+
 source $(poetry env info --path)/bin/activate
-#echo  ". ./launch.sh"
+
+# echo  ". ./launch.sh"
 # source ~/.zshrc
-#source venv/bin/activate
+# source venv/bin/activate
 
 # pipx install poetry (indepent of py version)
 # poetry init
 # poetry env use python3.12
-# source $(poetry env info --path)/bin/activate
-# poetry env activate
+# source $(poetry env info --path)/bin/activate   # (poetry env activate)
+# deactivate
 # python == python3.12
+
 # poetry add module_name
